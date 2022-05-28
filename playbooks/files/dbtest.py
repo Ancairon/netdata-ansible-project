e70278a849fd44d6e061eaedbeee135fd25e6af3
Fotis Voutsas <fotis@netdata.cloud>
2022-05-28 19:51:14 +0300

    changes to both watcher and db, experimenting with watch periods

--- playbooks/files/dbtest.py
+++ playbooks/files/dbtest.py
@@ -9,51 +9,47 @@ import threading
 def normalJob():
     #for i in range(0, 1):
 
-        try:
-            conn = mariadb.connect(
-                user="root",
-                password="pass",
-                host="192.168.1.60",
-                port=3306,
-                database="employees"
+    try:
+        conn = mariadb.connect(user="root",
+                               password="pass",
+                               host="192.168.1.60",
+                               port=3306,
+                               database="employees")
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        #continue
+        return
 
-            )
-        except mariadb.Error as e:
-            print(f"Error connecting to MariaDB Platform: {e}")
-            #continue
-            return
+    # Get Cursor
+    cur = conn.cursor()
 
-        # Get Cursor
-        cur = conn.cursor()
+    cur.execute("SELECT first_name, last_name FROM employees")
 
-        cur.execute(
-            "SELECT first_name, last_name FROM employees")
-
-        for (first_name, last_name) in cur:
-            print(f"First Name: {first_name}, Last Name: {last_name}")
+    for (first_name, last_name) in cur:
+        print(f"First Name: {first_name}, Last Name: {last_name}")
 
 
 def buggyJob():
     # for i in range(0, 5):
 
     try:
-        conn = mariadb.connect(
-            user="root",
-            password="pass",
-            host="192.168.1.61",
-            port=3306,
-            database="employees"
-
-        )
+        conn = mariadb.connect(user="root",
+                               password="pass",
+                               host="192.168.1.61",
+                               port=3306,
+                               database="employees")
     except mariadb.Error as e:
         print(f"Error connecting to MariaDB Platform: {e}")
+        sleep(3)
     return
 
+
 counter = 0
+k = 0
 
 for k in range(0, 50):
-    #random.randint(0, 30)
-    if (k % 3 == 0) & k > 10:
+    
+    if (k % random.randint(1, 10) == 0):
         buggyJob()
         counter += 1
     else:
--- playbooks/files/old_watcher.py
+++ playbooks/files/old_watcher.py
@@ -1,7 +1,5 @@
 import requests
 import scipy.stats as sp
-import time
-from datetime import datetime
 
 
 def logToFile(line):
@@ -11,26 +9,26 @@ def logToFile(line):
 def watch(chart, dimension):
     r = requests.get(
         'http://localhost:19999/api/v1/data?chart={}&dim'
-        'ension={}&after=-3&before=0&points=3&group=average&gtime=0&format=json&options=seconds&options'
+        'ension={}&after=-60&before=0&points=60&group=average&gtime=0&format=json&options=seconds&options'
         '=jsonwrap'.format(chart, dimension))
 
     a = r.json()['result']['data']
     last_metrics = []
 
-    for i in range(3):
+    for i in range(60):
         last_metrics.append(a[i][1])
 
     last_metrics.reverse()
 
     r = requests.get(
         'http://localhost:19999/api/v1/data?chart={}&dim'
-        'ension={}&after=-23&before=-3&points=20&group=average&gtime=0&format=json&options=seconds&options'
+        'ension={}&after=-600&before=-120&points=480&group=average&gtime=0&format=json&options=seconds&options'
         '=jsonwrap'.format(chart, dimension))
 
     b = r.json()['result']['data']
     metrics = []
 
-    for i in range(20):
+    for i in range(480):
         metrics.append(b[i][1])
 
     metrics.reverse()
@@ -45,19 +43,15 @@ def watch(chart, dimension):
     alpha = 0.10
 
     if p_value <= alpha:
-        now = datetime.now().strftime("%c")
         print("not equal, ANOMALY DETECTED on chart:", chart, "and dimension:", dimension)
-        logToFile("not equal, ANOMALY DETECTED on chart: {chart} and dimension: {dimension}, Timestamp: {now}")
-        
-    #else:
-        #print("no significant difference on chart:", chart, "and dimension:", dimension)
-        #logToFile("no significant difference on chart: {chart} and dimension: {dimension}")
+        logToFile("not equal, ANOMALY DETECTED on chart: {chart} and dimension: {dimension}")
+    else:
+        print("no significant difference on chart:", chart, "and dimension:", dimension)
+        logToFile("no significant difference on chart: {chart} and dimension: {dimension}")
 
 
-while(True):
-    watch("system.cpu", "user")
+watch("system.cpu", "user")
 
-    # laptop watch("net.wlp1s0", "received")
+# laptop watch("net.wlp1s0", "received")
 
-    watch("net.enp0s3", "received")
-    time.sleep(1)
+watch("net.enp0s3", "received")
